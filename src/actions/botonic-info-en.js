dae38a0ba6d1b6ee1ab744cad740a6d0025f9a56
Paula Boyano <paula.boyano@est.fib.upc.edu>
2020-08-01 16:12:59 +0200

    botonic-info-en action added

---
+++ src/actions/botonic-info-en.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {
+    Text,
+    Button,
+    Image
+} from '@botonic/react'
+export default class extends React.Component {
+    render() {
+        return (
+            <>
+                <Text>Botonic is an easy to learn, open-source framework that lets you create powerful conversational interfaces.</Text>
+                <Image src='https://botonic.io/images/botonic_react_logo-p-500.png'> </Image>
+                <Button url='https://botonic.io'>Visitar la web de Botonic</Button>
+
+            </>
+        )
+    }
+}
