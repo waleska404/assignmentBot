e456b236c89a99428cc5b707fcf313aa9a441450
Paula Boyano <paula.boyano@est.fib.upc.edu>
2020-08-01 16:09:20 +0200

    author-info-en action added

--- src/actions/author-info-en.js
+++ src/actions/author-info-en.js
@@ -8,13 +8,13 @@ export default class extends React.Component {
     render() {
         return (
             <>
-                <Text>La autora de este bot es Paula Boyano, una estudiante de ingeniería informática en la UPC.
-                    Le apasiona la tecnología, puedes ver algunos de sus proyectos en su github:
+                <Text>The author of this bot is Paula Boyano, a computer engineering student at UPC.
+                    She is passionate about technology, you can see some of her projects on her github page:
                 </Text>
                 <Image
                     src='https://avatars3.githubusercontent.com/u/26300667?s=400&u=f513fa8e8734e632477744526c60c5bd9651798e&v=4'>
                 </Image>
-                <Button url='https://github.com/waleska404'>Visitar el Github de Paula</Button>
+                <Button url='https://github.com/waleska404'>Visit Paula's Github</Button>
 
             </>
         )
