7f49f62f2862e53ad60e67ad9d95629da8bf1e3d
Paula Boyano <paula.boyano@est.fib.upc.edu>
2020-08-01 16:15:51 +0200

    help-en action added

---
+++ src/actions/help-en.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Text } from '@botonic/react'
+
+export default class extends React.Component {
+    render() {
+        return (
+            <>
+                <Text>
+                    To interact with this bot you can:
+                    Type "hubtype-info" if you want more information about Hubtype.
+                    Type "botonic-info" if you want more information about Botonic.
+                    Type "author" if you want to know more about the author of this bot.
+                    Type "help" if you want to know what you can do here.
+                </Text>
+            </>
+        )
+    }
+}
