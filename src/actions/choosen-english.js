d198758076989a799b91f9d8c09697b5ccec6432
Paula Boyano <paula.boyano@est.fib.upc.edu>
2020-08-01 17:53:31 +0200

    choosen-english action test added

--- src/actions/choosen-english.js
+++ src/actions/choosen-english.js
@@ -5,9 +5,9 @@ export default class extends React.Component {
     render() {
         return (
             <>
-                <Text>Great!</Text>
                 <Text>
-                    Hi {this.props.name}, welcome, this is a bot developed (using Botonic) by Paula Boyano as an asignment for Hubtype.
+                    Great! Hi {this.props.name}, welcome, this is a bot developed
+                    (using Botonic) by Paula Boyano as an asignment for Hubtype.
                     If you want to know what you can do here, type "help".
                 </Text>
             </>
--- tests/app.test.js
+++ tests/app.test.js
@@ -10,6 +10,17 @@ const app = new NodeApp({ routes, locales, plugins, ...config })
 const i = new BotonicInputTester(app)
 const o = new BotonicOutputTester(app)
 
+test('TEST: choosen-english.js', async () => {
+  await expect(i.payload('english')).resolves.toBe(
+      o.text(
+          'Great! Hi {this.props.name}, welcome, this is a bot developed ' +
+          '(using Botonic) by Paula Boyano as an asignment for Hubtype.' +
+          'If you want to know what you can do here, type "help".'
+      )
+  )
+})
+
+
 test('TEST: (404) NOT FOUND', async () => {
   await expect(i.text('whatever')).resolves.toBe(
     o.text("I don't understand you")
